56bd2afa32aae6f75ad4eeaa073d6afe1d7eba9d
Richard Fullmer <richardfullmer@gmail.com>
2012-02-17 07:18:28 -0800

    Credentials aren't serialized in the Token, so subsequent calls to getPassword fail after the token is serialized to the session.
    Since the attributes themselves __are__ serialized, move the credentials field into the attributes

--- Authentication/Token/LdapToken.php
+++ Authentication/Token/LdapToken.php
@@ -6,21 +6,19 @@ use Symfony\Component\Security\Core\Authentication\Token\AbstractToken;
 
 class LdapToken extends AbstractToken
 {
-  private 
-    $credentials;
 
   public function __construct($username, $password, array $roles= array())
   {
     parent::__construct($roles);
 
     $this->setuser($username);
-    $this->credentials = $password;
+    $this->setAttribute('credentials', $password);
   }
 
   public function getCredentials()
   {
-    return $this->credentials;
+    return $this->getAttribute('credentials');
   }
 
-  
+
 }
