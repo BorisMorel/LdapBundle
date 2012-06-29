1683eaf814f361bb05e8505e76057af47b8c2854
Juti Noppornpitak <jutin@nationalfibre.net>
2012-06-29 16:09:55 -0400

    Updated the code to SF coding standard, including adding comments.

--- Provider/LdapUserProvider.php
+++ Provider/LdapUserProvider.php
@@ -2,58 +2,76 @@
 
 namespace IMAG\LdapBundle\Provider;
 
-use Symfony\Component\Security\Core\User\UserProviderInterface,
-  Symfony\Component\Security\Core\User\UserInterface,
-  IMAG\LdapBundle\Manager\LdapManagerUserInterface,
-  Symfony\Component\Security\Core\Exception\UsernameNotFoundException,
-  Symfony\Component\Security\Core\Exception\UnsupportedUserException,
-  IMAG\LdapBundle\User\LdapUser;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException,
+    Symfony\Component\Security\Core\Exception\UsernameNotFoundException,
+    Symfony\Component\Security\Core\User\UserInterface,
+    Symfony\Component\Security\Core\User\UserProviderInterface;
 
+use IMAG\LdapBundle\Manager\LdapManagerUserInterface,
+    IMAG\LdapBundle\User\LdapUser;
+
+/**
+ * LDAP User Provider
+ *
+ * @author Boris Morel
+ * @author Juti Noppornpitak <jnopporn@shiroyuki.com>
+ */
 class LdapUserProvider implements UserProviderInterface
 {
-  private
-    $ldapManager;
+    private $ldapManager;
 
-  public function __construct(LdapManagerUserInterface $ldapManager)
-  {
-    $this->ldapManager = $ldapManager;
-  }
-
-  public function loadUserByUsername($username)
-  {
-    if (empty($username)) {
-        throw new UsernameNotFoundException('The username is not provided.');
+    public function __construct(LdapManagerUserInterface $ldapManager)
+    {
+        $this->ldapManager = $ldapManager;
     }
 
-    if(!$this->ldapManager->exists($username))
-        throw new UsernameNotFoundException(sprintf('User "%s" not found', $username));
+    /**
+     * {@inheritdoc}
+     */
+    public function loadUserByUsername($username)
+    {
+        // Throw the exception if the username is not provided.
+        if (empty($username)) {
+            throw new UsernameNotFoundException('The username is not provided.');
+        }
 
-    $lm = $this->ldapManager
-      ->setUsername($username)
-      ->doPass();
+        // Throw the exception if the username is not found.
+        if(!$this->ldapManager->exists($username)) {
+            throw new UsernameNotFoundException(sprintf('User "%s" not found', $username));
+        }
 
-    $ldapUser = new LdapUser();
-    $ldapUser
-      ->setUsername($lm->getUsername())
-      ->setEmail($lm->getEmail())
-      ->setRoles($lm->getRoles())
-      ->setDn($lm->getDn())
-      ->setAttributes($lm->getAttributes())
-     ;
+        $lm = $this->ldapManager
+            ->setUsername($username)
+            ->doPass();
 
-    return $ldapUser;
-  }
+        $ldapUser = new LdapUser();
+        $ldapUser
+            ->setUsername($lm->getUsername())
+            ->setEmail($lm->getEmail())
+            ->setRoles($lm->getRoles())
+            ->setDn($lm->getDn())
+            ->setAttributes($lm->getAttributes());
 
-  public function refreshUser(UserInterface $user)
-  {
-    if(!$user instanceof LdapUser)
-      throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', get_class($user)));
+        return $ldapUser;
+    }
 
-    return $this->loadUserByUsername($user->getUsername());
-  }
+    /**
+     * {@inheritdoc}
+     */
+    public function refreshUser(UserInterface $user)
+    {
+        if (!$user instanceof LdapUser) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', get_class($user)));
+        }
 
-  public function supportsClass($class)
-  {
-    return (bool) ($class === 'IMAG\LdapBundle\User\LdapUser');
-  }
+        return $this->loadUserByUsername($user->getUsername());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsClass($class)
+    {
+        return $class === 'IMAG\LdapBundle\User\LdapUser';
+    }
 }
