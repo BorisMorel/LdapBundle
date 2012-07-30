7264358d5b6623330521ee21428f105e72406c8c
Boris Morel <Boris.Morel@imag.fr>
2012-07-30 11:05:39 +0200

    Credentials mustn't store into the attribute of token & the provider key must be accesible on the token

--- Authentication/Token/LdapToken.php
+++ Authentication/Token/LdapToken.php
@@ -7,18 +7,41 @@ use Symfony\Component\Security\Core\Authentication\Token\AbstractToken;
 class LdapToken extends AbstractToken
 {
 
-  public function __construct($username, $password, array $roles= array())
-  {
-    parent::__construct($roles);
+    public function __construct($username, $password, $providerKey, array $roles= array())
+    {
+        parent::__construct($roles);
 
-    $this->setuser($username);
-    $this->setAttribute('credentials', $password);
-  }
+        $this->setuser($username);
+        $this->credentials = $password;
+        $this->providerKey = $providerKey;
+    }
 
-  public function getCredentials()
-  {
-    return $this->getAttribute('credentials');
-  }
+    public function getCredentials()
+    {
+        return $this->credentials;
+    }
 
+    public function getProviderKey()
+    {
+        return $this->providerKey;
+    }
 
+    public function eraseCredentials()
+    {
+        parent::eraseCredentials();
+
+        $this->credentials = null;
+    }
+
+    public function serialize()
+    {
+        return serialize(array($this->credentials, $this->providerKey, parent::serialize()));
+    }
+
+    public function unserialize($str)
+    {
+        list($this->credentials, $this->providerKey, $parentStr) = unserialize($str);
+        parent::unserialize($parentStr);
+    }
+    
 }
