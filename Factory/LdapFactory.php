f06fe2a32dd853b0dac0dd1c727ce602a1bfb9eb
Boris Morel <Boris.Morel@imag.fr>
2013-03-06 12:05:26 +0100

    Inject the daoProvider to use the chain provider

--- Factory/LdapFactory.php
+++ Factory/LdapFactory.php
@@ -10,77 +10,86 @@ use Symfony\Bundle\SecurityBundle\DependencyInjection\Security\Factory\AbstractF
 
 class LdapFactory extends AbstractFactory
 {
-  public function __construct()
-  {
-    $this->addOption('username_parameter', '_username');
-    $this->addOption('password_parameter', '_password');
-    $this->addOption('csrf_parameter', '_csrf_token');
-    $this->addOption('intention', 'ldap_authenticate');
-    $this->addOption('post_only', true);
-  }
-
-  public function getPosition()
-  {
-    return 'form';
-  }
-
-  public function getKey()
-  {
-    return 'imag_ldap';
-  }
-
-  public function addConfiguration(NodeDefinition $node)
-  {
-    parent::addConfiguration($node);
-    
-    $node
-      ->children()
-        ->scalarNode('csrf_provider')->cannotBeEmpty()->end()
-      ->end()
-      ;
-  }
-
-  protected function getListenerId()
-  {
-    return 'imag_ldap.security.authentication.listener';
-  }
-
-  protected function createAuthProvider(ContainerBuilder $container, $id, $config, $userProviderId)
-  {
-    $provider = 'imag_ldap.security.authentication.provider.'.$id;
-
-    $container
-      ->setDefinition($provider, new DefinitionDecorator('imag_ldap.security.authentication.provider'))
-      ->replaceArgument(0, new Reference($userProviderId))
-      ->replaceArgument(3, $id)
-      ;
-
-    return $provider;
-  }
-  
-  protected function createlistener($container, $id, $config, $userProvider)
-  {
-    $listenerId = parent::createListener($container, $id, $config, $userProvider);
-
-    if (isset($config['csrf_provider'])) {
-      $container
-        ->getDefinition($listenerId)
-        ->addArgument(new Reference($config['csrf_provider']))
-        ;
+    public function __construct()
+    {
+        $this->addOption('username_parameter', '_username');
+        $this->addOption('password_parameter', '_password');
+        $this->addOption('csrf_parameter', '_csrf_token');
+        $this->addOption('intention', 'ldap_authenticate');
+        $this->addOption('post_only', true);
     }
+
+    public function getPosition()
+    {
+        return 'form';
+    }
+
+    public function getKey()
+    {
+        return 'imag-ldap';
+    }
+
+    public function addConfiguration(NodeDefinition $node)
+    {
+        parent::addConfiguration($node);
     
-    return $listenerId;
-  }
+        $node
+            ->children()
+                ->scalarNode('csrf_provider')->cannotBeEmpty()->end()
+            ->end()
+            ;
+    }
 
-  protected function createEntryPoint($container, $id, $config, $defaultEntryPoint)
-  {
-    $entryPointId = 'imag_ldap.security.authentication.form_entry_point.'.$id;
-    $container
-      ->setDefinition($entryPointId, new DefinitionDecorator('imag_ldap.security.authentication.form_entry_point'))
-      ->addArgument($config['login_path'])
-      ->addArgument($config['use_forward'])
-      ;
+    protected function getListenerId()
+    {
+        return 'imag_ldap.security.authentication.listener';
+    }
 
-    return $entryPointId;
-  }
+    protected function createAuthProvider(ContainerBuilder $container, $id, $config, $userProviderId)
+    {
+
+        $dao = 'security.authentication.provider.dao.'.$id;
+        $container
+            ->setDefinition($dao, new DefinitionDecorator('security.authentication.provider.dao'))
+            ->replaceArgument(0, new Reference($userProviderId))
+            ->replaceArgument(2, $id)
+        ;
+
+        $provider = 'imag_ldap.security.authentication.provider.'.$id;
+        $container
+            ->setDefinition($provider, new DefinitionDecorator('imag_ldap.security.authentication.provider'))
+            ->replaceArgument(0, new Reference($userProviderId))
+            ->replaceArgument(1, new Reference($dao))
+            ->replaceArgument(4, $id)
+            ;
+
+        return $provider;
+    }
+  
+    protected function createlistener($container, $id, $config, $userProvider)
+    {
+        $listenerId = parent::createListener($container, $id, $config, $userProvider);
+
+        if (isset($config['csrf_provider'])) {
+            $container
+                ->getDefinition($listenerId)
+                ->addArgument(new Reference($config['csrf_provider']))
+                ;
+        }
+    
+        return $listenerId;
+    }
+
+    protected function createEntryPoint($container, $id, $config, $defaultEntryPoint)
+    {
+        $entryPointId = 'imag_ldap.security.authentication.form_entry_point.'.$id;
+        $container
+            ->setDefinition($entryPointId, new DefinitionDecorator('imag_ldap.security.authentication.form_entry_point'))
+            ->addArgument(new Reference('security.http_utils'))
+            ->addArgument($config['login_path'])
+            ->addArgument($config['use_forward'])
+            ;
+      
+        return $entryPointId;
+    }
 }
