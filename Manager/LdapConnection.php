90a79e81313e0ffbff187498308cd30465efa37d
dsteiner23 <daniel.steiner@gmx.de>
2014-08-09 09:47:21 +0200

    Data has to be encoded, since the logger is calling an json_encode(). Without encoding it will crash with german umlauts

--- Manager/LdapConnection.php
+++ Manager/LdapConnection.php
@@ -39,8 +39,8 @@ class LdapConnection implements LdapConnectionInterface
 
         $this->info(
             sprintf('ldap_search base_dn %s, filter %s',
-                    print_r($params['base_dn'], true),
-                    print_r($params['filter'], true)
+                print_r(utf8_encode($params['base_dn']), true),
+                print_r(utf8_encode($params['filter']), true)
             ));
 
         $search = @ldap_search(
